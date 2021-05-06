d624bea2129c6bf94f04c6aadd89a0738a9de71e
Anna Takeuchi <anna_take@hotmail.com>
2021-05-05 23:36:02 -0700

    Implement event handlers for buttons, updating result

--- main.js
+++ main.js
@@ -2,8 +2,29 @@ const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 const rockPaperScissorArr = [rock, paper, scissors];
-let playerPoints = 0;
-let computerPoints = 0;
+const GAMES_TO_PLAY = 5;
+let playerPointCount = 0;
+let computerPointCount = 0;
+let round = 0;
+
+// get button elements
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorBtn = document.getElementById("scissor-btn");
+
+// get text elements to update
+const gameResult = document.querySelector(".win-lose-result");
+const roundCount = document.querySelector(".round-count");
+const playerPoints = document.getElementById(".player-points");
+const computerPoints = document.getElementById(".computer-points");
+
+
+// Event Listeners
+document.addEventListener('DOMContentLoaded', gameReset);
+rockBtn.addEventListener("click", handleSelection);
+paperBtn.addEventListener("click", handleSelection);
+scissorBtn.addEventListener("click", handleSelection);
+
 
 function computerPlay() {
     let randValue = Math.floor(Math.random() * rockPaperScissorArr.length);
@@ -12,23 +33,22 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let player = playerSelection.toLowerCase();
-    console.log(`Player chose ${player}\nComputer chose ${computerSelection}`);
+    console.log(`Player chose ${playerSelection}\nComputer chose ${computerSelection}`);
     let result;
 
     switch (true) {
-        case (player ===  computerSelection):
-            result = `Both chose ${player}, so it's a tie!`;
+        case (playerSelection ===  computerSelection):
+            result = `It's a tie!`;
             break;
-        case (player === rock && computerSelection === scissors):
-        case (player === paper && computerSelection === rock):
-        case (player === scissors && computerSelection === paper):
-            result = `You win, ${player} beats ${computerSelection}!`;
-            playerPoints++;
+        case (playerSelection === rock && computerSelection === scissors):
+        case (playerSelection === paper && computerSelection === rock):
+        case (playerSelection === scissors && computerSelection === paper):
+            result = `You win, ${playerSelection} beats ${computerSelection}!`;
+            playerPointCount++;
             break;
         default:
-            result = `You lost, ${computerSelection} beats ${player}!`;
-            computerPoints++;
+            result = `You lost, ${computerSelection} beats ${playerSelection}!`;
+            computerPointCount++;
             break;
     }
     return result;
@@ -36,7 +56,6 @@ function playRound(playerSelection, computerSelection) {
 
 function playGame() {
     gameReset();
-    const GAMES_TO_PLAY = 5;
     for (let round = 1; round <= GAMES_TO_PLAY; round++) {
         console.log("Round " + round);
         let computerSelection = computerPlay();
@@ -47,13 +66,13 @@ function playGame() {
             validInput = validateInput(playerSelection);
         }
         console.log(playRound(playerSelection, computerSelection));
-        console.log(`Your score: ${playerPoints} \nComputer's score: ${computerPoints}`);
+        console.log(`Your score: ${playerPointCount} \nComputer's score: ${computerPointCount}`);
     }
     console.log("%c~ * ~ FINAL RESULTS ~ * ~", `
                 color: pink;
                 font-weight: bold;`);
-    console.log(`Your score: ${playerPoints} \nComputer's score ${computerPoints}`);
-    if (playerPoints > computerPoints) {
+    console.log(`Your score: ${playerPointCount} \nComputer's score ${computerPointCount}`);
+    if (playerPointCount > computerPointCount) {
         console.log("%cYou win!!! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)", "color: green;");
     } else if (playerPoints === computerPoints) {
         console.log("%cIt's a tie! (☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜) ", "color: blue;");
@@ -63,8 +82,9 @@ function playGame() {
 } 
 
 function gameReset() {
-    playerPoints = 0;
-    computerPoints = 0;
+    playerPointCount = 0;
+    computerPointCount = 0;
+    round = 0;
 }
 
 function validateInput(playerSelection) {
@@ -74,4 +94,15 @@ function validateInput(playerSelection) {
     return (rockPaperScissorArr.includes(playerSelection))? true : false;
 }
 
-document.getElementById("play-button").addEventListener("click", playGame);
+function handleSelection(e) {
+    const playerSelection = e.target.value;
+    let computerSelection = computerPlay();
+    if (round !== GAMES_TO_PLAY) {
+        let result = playRound(playerSelection, computerSelection);
+        console.log(gameResult);
+        gameResult.innerText = result;
+    }
+}
+
+
+//document.getElementById("play-button").addEventListener("click", playGame);
